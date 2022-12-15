353d6be8cd95a087dd0de179b694b37ba8dac976
Patrick Navarro <patricknv230101@gmail.com>
2022-12-15 13:31:20 -0500

    Ejercicio Práctico para aprender sintaxis básica de POO en C# .NET 6

---
+++ EjercicioPractico_POO/Persona.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPractico_POO
+{
+    internal class Persona
+    {
+        private const char generoDefecto = 'H';
+
+        private String nombre = "";
+        private int edad = 0;
+        private String dni;
+        private char genero = generoDefecto;
+        private double peso = 0;
+        private double altura = 0;
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(String nombre, int edad, char genero)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.genero = genero;
+        }
+
+        public Persona(string nombre, int edad, string dni, char genero, double peso, double altura)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.dni = dni;
+            this.genero = genero;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public Persona(string nombre, int edad, char genero, double peso, double altura)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.genero = genero;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public int calcularIMC()
+        {
+            int valorPesoIdeal = 0;
+            double pesoIdeal = (this.peso / (Math.Pow(this.altura / 100, 2)));
+            if(pesoIdeal < 20)
+            {
+                valorPesoIdeal =  -1;
+            }else if(pesoIdeal >= 20 && pesoIdeal < 25)
+            {
+                valorPesoIdeal = 0;
+            }else if(pesoIdeal > 25)
+            {
+                valorPesoIdeal = 1;
+            }
+            return valorPesoIdeal;
+        }
+
+        public void esMayorDeEdad()
+        {
+            if(this.edad >= 18)
+            {
+                Console.WriteLine(this.nombre + " es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine(this.nombre + " es menor de edad");
+            }
+        }
+
+        public void comprobarGenero(char genero)
+        {
+            if(genero == 'H' || genero == 'M')
+            {
+                Console.WriteLine("Genero de " + this.nombre + " es: " + genero);
+            }
+            else
+            {
+                genero = this.genero;
+                Console.WriteLine("Genero por defecto de " + this.nombre + " es: " + this.genero);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Nombre -> " + this.nombre + 
+                " - Edad -> " + this.edad + 
+                " - Dni -> " + this.dni + 
+                " - Genero -> " + this.genero + 
+                " - Peso -> " + this.peso + " kg"+ 
+                " - Altura -> " + this.altura+ " cm";
+        }
+
+        public void setNombre(String nombre)
+        {
+            this.nombre = nombre;   
+        }
+
+        public void setEdad(int edad)
+        {
+            this.edad  = edad;
+        }
+
+        public void setGenero(char genero)
+        {
+            this.genero = genero;
+        }
+
+        public void setPeso(double peso)
+        {
+            this.peso = peso;
+        }
+
+        public void setAltura(double altura)
+        {
+            this.altura = altura;
+        }
+
+        public String getNombre()
+        {
+            return this.nombre;
+        }
+
+    }
+}
---
+++ EjercicioPractico_POO/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace EjercicioPractico_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el nombre: ");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Introduzca la edad: ");
+            int edad = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el género: ");
+            char genero = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Introduzca el peso en kg: ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduzca la altura en cm: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Persona persona1 = new Persona(nombre, edad, genero, peso, altura);
+            Persona persona2 = new Persona(nombre, edad, genero);
+            Persona persona3 = new Persona();
+            persona3.setNombre("Enrique Segoviano");
+            persona3.setEdad(25);
+            persona3.setGenero('H');
+            persona3.setPeso(80.5);
+            persona3.setAltura(170);
+
+            validarIMC(persona1.getNombre(), persona1.calcularIMC());
+            validarIMC(persona2.getNombre(),persona2.calcularIMC());
+            validarIMC(persona3.getNombre(), persona3.calcularIMC());
+
+            persona1.esMayorDeEdad();
+            persona2.esMayorDeEdad();
+            persona3.esMayorDeEdad();
+
+            Console.WriteLine(persona1);
+            Console.WriteLine(persona2);
+            Console.WriteLine(persona3);
+        }
+
+        static void validarIMC(String nombre, int valor)
+        {
+            if(valor == -1)
+            {
+                Console.WriteLine(nombre+ " está bajo de peso");
+            }else if(valor == 0)
+            {
+                Console.WriteLine(nombre+ " está en tu peso ideal");
+            }else if(valor == 1)
+            {
+                Console.WriteLine(nombre+ " está con sobrepeso");
+            }
+        }
+    }
+}
+
